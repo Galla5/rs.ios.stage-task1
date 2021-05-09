a82ec55a56ca9d570edf42f801be1da5f87b406e
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-09 20:25:40 +0300

    feat: NumberConverter solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,16 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    if (number == nil) {
+        return @[];
+    }
+    NSNumber *positiveNumber = [NSNumber numberWithDouble:fabs([number doubleValue])];
+    NSMutableArray *characters = [[NSMutableArray alloc] initWithCapacity:[[positiveNumber stringValue] length]];
+    for (int i=0; i < [[positiveNumber stringValue] length]; i++) {
+        NSString *ichar  = [NSString stringWithFormat:@"%c", [[positiveNumber stringValue] characterAtIndex:i]];
+        [characters insertObject:ichar atIndex:0];
+    }
+    return characters;
 }
 
 @end
