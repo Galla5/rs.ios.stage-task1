4506635b18b103b764d29e53915ac4733752ec7c
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-05-09 20:20:25 +0300

    feat: ValidNumber solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,17 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    NSNumberFormatter* numberFormatter = [[NSNumberFormatter alloc] init];
+    NSNumber* number = [numberFormatter numberFromString:self];
+    if (number != nil) {
+        if (0 <= [self length] && [self length] <= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 @end
